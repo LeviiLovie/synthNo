674f75c324de54c9d9c7dd3963aaca9f09c5dbde
Lev111111 <leviiloviee@gmail.com>
2022-06-28 17:53:30 +0700

    all

---
+++ GUI/GUI.java
@@ -0,0 +1,13 @@
+// package GUI;
+
+// import java.awt.*;
+// import java.awt.event.*;
+// import javax.swing.*;
+
+// public static void main(String[] args) {
+//     JFrame frame = new JFrame(/* название нашей игры */);
+//     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//     frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+//     frame.setUndecorated(true);
+//     frame.setVisible(true);
+// }
--- index.java
+++ index.java
@@ -1,5 +1,13 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
 public class index {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
+		JFrame frame = new JFrame(/* название нашей игры */);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+		frame.setUndecorated(true);
+		frame.setVisible(true);
+	}
 }
